b0d5cf2bf029ed1ba8ece4643366bd1720832b2f
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2021-12-17 00:30:48 +0600

    card added

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import "./MovieCard.scss";
 
-const MovieCard = () => {
+const MovieCard = (props) => {
+    const { data } = props;
+
     return (
-        <div>
-            <h2>Movie Card</h2>
+        <div className='card-item'>
+            <div className="card-inner">
+                <div className="card-top">
+                    <img src={data.Poster} alt={data.title} />
+                </div>
+                <div className="card-bottom">
+                    <div className="card-info">
+                        <h4>{data.Title}</h4>
+                        <p>{data.year}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
